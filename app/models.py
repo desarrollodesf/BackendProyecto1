0b85e457c2a07f45ab3d0089e5d895928e9bb1dc
Rafael Eduardo Ropero Layton <rropero@unal.edu.co>
2022-02-06 21:00:22 -0500

    Start DB
    Commit inicial con mockup de BD! Pendiente hacer pruebas

--- app/models.py
+++ app/models.py
@@ -5,13 +5,14 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(160), index=True, unique=True)
+    name = db.Column(db.String(160), index=True)
+    lastname = db.Column(db.String(160), index=True)
     email = db.Column(db.String(160), index=True, unique=True)
     password_hash = db.Column(db.String(160))
-    posts = db.relationship('Post', backref='author', lazy='dynamic')
+    posts = db.relationship('Contest', backref='User', lazy='dynamic')
 
     def __repr__(self):
-        return '<User {}>'.format(self.username)
+        return '<User {}>'.format(self.email)
     
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -23,18 +24,31 @@ class User(UserMixin, db.Model):
 def load_user(id):
     return User.query.get(int(id))
 
-class Post(db.Model):
+class Contest(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(160))
-    category = db.Column(db.String(160))
-    place = db.Column(db.String(160))
+    banner = db.Column(db.String(160))
+    url = db.Column(db.String(160))
+    startDate = db.Column(db.DateTime, index=True)
+    endDate = db.Column(db.DateTime, index=True)
+    payment = db.Column(db.Integer)
+    script = db.Column(db.String(160))
     address = db.Column(db.String(160))
-    timestamp = db.Column(db.DateTime, index=True)
-    timestamp_end = db.Column(db.DateTime, index=True)
-    method = db.Column(db.String(160))
+    notes = db.Column(db.String(160))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
 
 
+class Form(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(160), index=True)
+    name = db.Column(db.String(160))
+    lastname = db.Column(db.String(160), index=True)
+    uploadDate = db.Column(db.DateTime, index=True)
+    state = db.Column(db.String(160))
+    original = db.Column(db.String(160))
+    formatted = db.Column(db.String(160))
+    notes = db.Column(db.String(160)) 
+    contest_id = db.Column(db.Integer, db.ForeignKey('contest.id'))
