d1306b967e5615cb679df1abeb6c8829e03a1afe
nirobe <n.rozo10@uniandes.edu.co>
2022-02-08 20:59:19 -0500

    se prueba servicios sin datos de tipo fecha

--- app/__init__.py
+++ app/__init__.py
@@ -19,7 +19,7 @@ bootstrap = Bootstrap(app)
 login = LoginManager(app)
 login.login_view = 'login'
 
-from app.models import Contest, Form
+from app.models import Contest, Form, User
 
 def setup_database(app):
     db.init_app(app)
@@ -45,38 +45,37 @@ class ContestResource(Resource):
 
     def put(self, contest_id):
 
-        error = []
         contest = Contest.query.get_or_404(contest_id)
 
         if 'name' in request.json:
-            contest.nombre = request.json['name']
+            contest.name = request.json['name']
 
         if 'banner' in request.json:
-            contest.lugar = request.json['banner']
+            contest.banner = request.json['banner']
 
         if 'url' in request.json:
-            contest.dirección = request.json['url']
+            contest.url = request.json['url']
 
-        if 'startDate' in request.json:
-            contest.fechaInicio = request.json['startDate']
+        #if 'startDate' in request.json:
+        #    contest.fechaInicio = request.json['startDate']
 
-        if 'endDate' in request.json:
-            contest.fechaFin = request.json['endDate']
+        #if 'endDate' in request.json:
+        #    contest.fechaFin = request.json['endDate']
 
         if 'payment' in request.json:
-            contest.categoria = request.json['payment']
+            contest.payment = request.json['payment']
 
         if 'script' in request.json:
-            contest.categoria = request.json['script']
+            contest.script = request.json['script']
         
         if 'address' in request.json:
-            contest.categoria = request.json['address']
+            contest.address = request.json['address']
 
         if 'notes' in request.json:
-            contest.categoria = request.json['notes']
+            contest.notes = request.json['notes']
 
         if 'user_id' in request.json:
-            contest.categoria = request.json['user_id']
+            contest.user_id = request.json['user_id']
         
         db.session.commit()
         return contest_schema.dump(contest)
@@ -92,16 +91,16 @@ class ContestResource(Resource):
 class ContestsResource(Resource):
 
     def get(self):
-        eventos = Contest.query.all()
-        return contests_schema.dump(eventos)
+        contests = Contest.query.all()
+        return contests_schema.dump(contests)
 
     def post(self):
             new_contest = Contest(
                 name = request.json['name'],
                 banner = request.json['banner'],
                 url = request.json['url'],
-                startDate = request.form['startDate'],
-                endDate = request.form['endDate'],
+                #startDate = request.form['startDate'],
+                #endDate = request.form['endDate'],
                 payment = request.json['payment'],
                 script = request.json['script'],
                 address = request.json['address'],
@@ -115,7 +114,7 @@ class ContestsResource(Resource):
 
 class Form_Schema(ma.Schema):
     class Meta:
-        fields = ("email", "name", "lastname", "uploadDate", "state", "original", "formatted", "notes", "contest_id")
+        fields = ("id","email", "name", "lastname", "uploadDate", "state", "original", "formatted", "notes", "contest_id")
 
 form_schema = Form_Schema()
 forms_schema = Form_Schema(many = True)
@@ -123,42 +122,42 @@ forms_schema = Form_Schema(many = True)
 class FormResource(Resource):
 
     def get(self, form_id):
-        contest = Form.query.get_or_404(form_id)
-        return contest_schema.dump(contest)
+        form = Form.query.get_or_404(form_id)
+        return form_schema.dump(form)
 
     def put(self, form_id):
 
         contest = Form.query.get_or_404(form_id)
 
         if 'email' in request.json:
-            contest.nombre = request.json['email']
+            contest.email = request.json['email']
 
         if 'name' in request.json:
-            contest.lugar = request.json['name']
+            contest.name = request.json['name']
 
         if 'lastname' in request.json:
-            contest.dirección = request.json['lastname']
+            contest.lastname = request.json['lastname']
 
-        if 'uploadDate' in request.json:
-            contest.fechaInicio = request.json['uploadDate']
+        #if 'uploadDate' in request.json:
+        #    contest.fechaInicio = request.json['uploadDate']
 
         if 'state' in request.json:
-            contest.fechaFin = request.json['state']
+            contest.state = request.json['state']
 
         if 'original' in request.json:
-            contest.categoria = request.json['original']
+            contest.original = request.json['original']
         
         if 'formatted' in request.json:
-            contest.categoria = request.json['formatted']
+            contest.formatted = request.json['formatted']
 
         if 'notes' in request.json:
-            contest.categoria = request.json['notes']
+            contest.notes = request.json['notes']
 
         if 'contest_id' in request.json:
-            contest.categoria = request.json['contest_id']
+            contest.contest_id = request.json['contest_id']
         
         db.session.commit()
-        return contest_schema.dump(contest)
+        return form_schema.dump(contest)
 
 
     def delete(self, form_id):
@@ -171,15 +170,15 @@ class FormResource(Resource):
 class FormsResource(Resource):
 
     def get(self):
-        eventos = Form.query.all()
-        return forms_schema.dump(eventos)
+        forms = Form.query.all()
+        return forms_schema.dump(forms)
 
     def post(self):
             new_form = Form(
                 email = request.json['email'],
                 name = request.json['name'],
                 lastname = request.json['lastname'],
-                uploadDate = request.form['uploadDate'],
+                #uploadDate = request.form['uploadDate'],
                 state = request.json['state'],
                 original = request.json['original'],
                 formatted = request.json['formatted'],
@@ -191,7 +190,39 @@ class FormsResource(Resource):
             db.session.commit()
             return form_schema.dump(new_form)
 
+class UserResource(Resource):
+    def post(self):
+        name = request.json['name']
+        lastname = request.json['lastname']
+        email = request.json['email']
+        password = request.json['password']
+
+        user = User.query.filter_by(email=email).first()
+        if user is not None:
+            return 'Usuario ya existe', 400
+
+        user_data = User(name=name, lastname=lastname,email=email )
+        user_data.set_password(password)
+        db.session.add(user_data)
+        db.session.commit()
+        return 'Usuario creado', 204
+
+class ContestsByUserResource(Resource):
+    
+    def get(self, user_id):
+        contests = Contest.query.filter_by(user_id=user_id)
+        return contests_schema.dump(contests)
+
+class FormsByContestResource(Resource):
+    
+    def get(self, contest_id):
+        contests = Contest.query.filter_by(contest_id=contest_id)
+        return contests_schema.dump(contests)
+
+api.add_resource(UserResource,'/api/administrador/')   
 api.add_resource(FormsResource,'/api/forms/')
 api.add_resource(FormResource,'/api/form/<int:form_id>')
 api.add_resource(ContestsResource,'/api/contests/')
 api.add_resource(ContestResource,'/api/contest/<int:contest_id>')
+api.add_resource(ContestsByUserResource,'/api/administrador/<int:user_id>/contests')
+api.add_resource(FormsByContestResource,'/api/contests/<int:user_id>/forms')
