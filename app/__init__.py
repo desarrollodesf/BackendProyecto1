c1e8c39895e4f629ca0206aafdad2f1695997955
nirobe <n.rozo10@uniandes.edu.co>
2022-03-10 21:34:49 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -108,7 +108,8 @@ class ContestResource(Resource):
             
             if f.filename != "":
                 #PATH_GUARDAR = "D:/Nirobe/202120-Grupo07/BackendProyecto1/imagen/" +  f.filename
-                PATH_GUARDAR = "/home/ubuntu/BackendProyecto1/imagen/"  +  f.filename
+                #PATH_GUARDAR = "/home/ubuntu/BackendProyecto1/imagen/"  +  f.filename
+                PATH_GUARDAR = "/var/locally-mounted/"  +  f.filename
                 contest.nombreBanner = f.filename
                 contest.banner = PATH_GUARDAR
                 f.save(PATH_GUARDAR)
@@ -141,7 +142,9 @@ class ContestsResource(Resource):
 
                 if not data['name']:
                     return 'No se puede dejar el nombre del concurso vacío', 400
-                PATH_GUARDAR = "/home/ubuntu/BackendProyecto1/imagen/"  +  data['nombreBanner']
+
+                PATH_GUARDAR = "/var/locally-mounted/"  +  data['nombreBanner']
+                #PATH_GUARDAR = "/home/ubuntu/BackendProyecto1/imagen/"  +  data['nombreBanner']
                 #PATH_GUARDAR = "D:/Nirobe/202120-Grupo07/BackendProyecto1/imagen/" +  data['nombreBanner']
 
                 new_contest = Contest(
@@ -233,7 +236,9 @@ class FormsResource(Resource):
         else: 
             numberFile = numberFile+1
         f = request.files['file']
-        PATH_GUARDAR = "/home/ubuntu/BackendProyecto1/files/"+  str(numberFile)   +  f.filename
+
+        PATH_GUARDAR = "/var/locally-mounted/"+  str(numberFile)   +  f.filename
+        #PATH_GUARDAR = "/home/ubuntu/BackendProyecto1/files/"+  str(numberFile)   +  f.filename
         #PATH_GUARDAR = "D:/Nirobe/202120-Grupo07/BackendProyecto1/files/" +  str(numberFile)  +  f.filename
 
         forms = Form.query.filter_by(original=PATH_GUARDAR).first()
@@ -334,7 +339,8 @@ class GetContestImageResource(Resource):
         contest = Contest.query.filter_by(id=contest_id).first()
         try:
             #return send_from_directory("D:/Nirobe/202120-Grupo07/BackendProyecto1/imagen/", contest.nombreBanner, as_attachment=True)
-            return send_from_directory("/home/ubuntu/BackendProyecto1/imagen/", contest.nombreBanner, as_attachment=True)
+           return send_from_directory("/var/locally-mounted/", contest.nombreBanner, as_attachment=True)
+           #return send_from_directory("/home/ubuntu/BackendProyecto1/imagen/", contest.nombreBanner, as_attachment=True)
         except FileNotFoundError:
             return(404)
 
@@ -343,7 +349,8 @@ class GetOriginalAudioResource(Resource):
         audio = Form.query.filter_by(id=form_id).first()
 
         try:
-            return send_from_directory("/home/ubuntu/BackendProyecto1/files/", os.path.basename(audio.original), as_attachment=True)
+            return send_from_directory("/var/locally-mounted/", os.path.basename(audio.original), as_attachment=True)
+            #return send_from_directory("/home/ubuntu/BackendProyecto1/files/", os.path.basename(audio.original), as_attachment=True)
         except FileNotFoundError:
             return(400)
 
@@ -351,7 +358,8 @@ class GetConvertedAudioResource(Resource):
     def get(self, form_id):
         audio = Form.query.filter_by(id=form_id).first()
         try:
-            return send_from_directory("/home/ubuntu/BackendProyecto1/files/", os.path.basename(audio.formatted), as_attachment=True)
+            return send_from_directory("/var/locally-mounted/", os.path.basename(audio.formatted), as_attachment=True)
+            #return send_from_directory("/home/ubuntu/BackendProyecto1/files/", os.path.basename(audio.formatted), as_attachment=True)
         except FileNotFoundError:
             return(400)
 
