7582305323c5b69dfa04235eeb39e60cfb03618c
nirobe <n.rozo10@uniandes.edu.co>
2022-04-22 10:22:12 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 import os.path
 
-
+from sqlalchemy.ext.declarative import DeclarativeMeta
 from flask import Flask, request, abort, jsonify, send_from_directory, flash,send_file,after_this_request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -248,10 +248,11 @@ class ContestsResource(Resource):
                     response = upload_file(f"uploads/{f.filename}", S3_BUCKET)
                     os.remove(PATH_GUARDAR)
                 db.session.commit()
+
+                print(r.set(new_contest.id),json.dumps(new_contest, cls=AlchemyEncoder))  
             except Exception as e:
                 return str(e), 400
 
-            print(r.set(new_contest.id),contest_schema.dump(new_contest))    
             return contest_schema.dump(new_contest)
 
 class Form_Schema(ma.Schema):
@@ -509,7 +510,24 @@ def sendMessageQueue(nombreArchivo, idForm, email, name ):
     )
     print(response)
 
-    
+class AlchemyEncoder(json.JSONEncoder):
+
+    def default(self, obj):
+        if isinstance(obj.__class__, DeclarativeMeta):
+            # an SQLAlchemy class
+            fields = {}
+            for field in [x for x in dir(obj) if not x.startswith('_') and x != 'metadata']:
+                data = obj.__getattribute__(field)
+                try:
+                    json.dumps(data) # this will fail on non-encodable values, like other classes
+                    fields[field] = data
+                except TypeError:
+                    fields[field] = None
+            # a json-encodable dict
+            return fields
+
+        return json.JSONEncoder.default(self, obj)
+
 api.add_resource(UserResource,'/api/administrador/')   
 api.add_resource(FormsResource,'/api/forms/')
 api.add_resource(FormResource,'/api/form/<int:form_id>')
