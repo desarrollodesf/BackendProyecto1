4ed26c573681f909a202cdc64450a63b9bb4627d
nirobe <n.rozo10@uniandes.edu.co>
2022-04-22 10:34:05 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -248,8 +248,8 @@ class ContestsResource(Resource):
                     response = upload_file(f"uploads/{f.filename}", S3_BUCKET)
                     os.remove(PATH_GUARDAR)
                 db.session.commit()
-
-                print(r.set(new_contest.id),json.dumps(new_contest, cls=AlchemyEncoder))  
+                print(json.dumps(jsonify(new_contest)))
+                print(r.set(new_contest.id),json.dumps(jsonify(new_contest)))  
             except Exception as e:
                 return str(e), 400
 
@@ -509,24 +509,6 @@ def sendMessageQueue(nombreArchivo, idForm, email, name ):
         MessageBody=json.dumps(message)
     )
     print(response)
-
-class AlchemyEncoder(json.JSONEncoder):
-
-    def default(self, obj):
-        if isinstance(obj.__class__, DeclarativeMeta):
-            # an SQLAlchemy class
-            fields = {}
-            for field in [x for x in dir(obj) if not x.startswith('_') and x != 'metadata']:
-                data = obj.__getattribute__(field)
-                try:
-                    json.dumps(data) # this will fail on non-encodable values, like other classes
-                    fields[field] = data
-                except TypeError:
-                    fields[field] = None
-            # a json-encodable dict
-            return fields
-
-        return json.JSONEncoder.default(self, obj)
 
 api.add_resource(UserResource,'/api/administrador/')   
 api.add_resource(FormsResource,'/api/forms/')
