0fdcc8ce86435cc79eb47879b123c44aa9ddea10
nirobe <n.rozo10@uniandes.edu.co>
2022-05-08 16:59:55 -0500

    make it better

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('postgresql://postgres:concursos@concursos.cw0necw4gli3.us-east-1.rds.amazonaws.com:5432/postgres')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
